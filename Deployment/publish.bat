25079a84d2b2279d6087809a9beb3d29bc8ac767
jehugaleahsa <jehugaleahsa@gmail.com>
2016-02-22 08:34:24 -0500

    NuGet updates and deployment

--- Deployment/publish.bat
+++ Deployment/publish.bat
@@ -1,4 +1,4 @@
-msbuild ../ChangeDetector.sln /p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild ../ChangeDetector.sln /p:Configuration=Release
 nuget pack ../ChangeDetector/ChangeDetector.csproj -Prop Configuration=Release
 nuget push *.nupkg
 del *.nupkg
